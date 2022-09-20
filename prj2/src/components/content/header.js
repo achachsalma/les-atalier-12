e7c3d538f30603cd260216d22c6559540cb7c842
achachsalma <jehhfjvfkbf@gmail.com>
2022-09-20 13:45:59 +0200

    first commit

---
+++ prj2/src/components/content/header.js
@@ -0,0 +1,7 @@
+const Header=()=>{
+    return <header>
+     <h1>
+        titre du site
+     </h1>
+      <h2> notre slogan</h2>
+    </header>
---
+++ prj2/src/components/content/navigation.js
@@ -0,0 +1,11 @@
+const Navigation=()=>{
+    return <nav>
+        <ul>
+            <li>Acceuil</li>
+            <li>item1</li>
+            <li>item2</li>
+            <li>item3</li>
+            <li>item4</li>
+        </ul>
+    </nav>
+}
